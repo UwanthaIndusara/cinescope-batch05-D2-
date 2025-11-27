3e571a2d5c851be17356a6ed185b878265b21d81
Uwantha Indusara <uwantha2021@gmail.com>
2025-11-28 00:08:58 +0530

    Add a new change

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 
 export default function Home() {
+  const myVariable = "Hello world";
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -12,6 +14,14 @@ export default function Home() {
           height={20}
           priority
         />
+
+        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
+          <li className="mb-2 tracking-[-0.01em]">{myVariable}</li>
+          <li className="tracking-[-0.01em]">
+            Save and see your changes instantly.
+          </li>
+        </ol>
+
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.
